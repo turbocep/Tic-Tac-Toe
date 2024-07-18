3f40ad6e2238a963646e261893fefd452cd950ea
turbocep <162988238+turbocep@users.noreply.github.com>
2024-07-18 12:18:48 +0200

    create Board class and paint method.

---
+++ main.rb
@@ -0,0 +1,26 @@
+#I'm doing a brain-dump of code all in one file. I'll sort into a 
+#nicer structure later.
+
+class Board
+  def initialize
+    @board = [[1,2,3], [4,5,6], [7,8,9]]
+  end
+
+  #Basic paint function is good enough for now while the main
+  #algorithm is the big priority. 
+  def paint
+    for i in 0..2 do
+      p @board[i]
+    end
+  end
+
+  #Writes to certain index. 
+end
+
+new_board = Board.new()
+
+new_board.paint
+
+new_board.board[1][1] = "X"
+
+new_board.paint
