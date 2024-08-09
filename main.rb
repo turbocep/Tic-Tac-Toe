d8fdce687cf39f9a58f9566054778b6ae8f806bf
turbocep <162988238+turbocep@users.noreply.github.com>
2024-08-09 13:07:40 +0200

    enable receiving input from both players
    I made the Game.play method call for user input from the Player class.
    Both players were instantiated within a "players" array to make toggling
    between them each turn more concise. Index variable i was added to
    control which player's turn it is to give input. I used a ternary
    operator to toggle between the players because it seemed intuitive.

--- main.rb
+++ main.rb
@@ -6,10 +6,34 @@ class Game
   def initialize
     @round = 1
     @board = Board.new([1, 2, 3, 4, 5, 6, 7, 8, 9])
-    @player1 = Player.new("player1", "X", false)
-    @player2 = Player.new("player2", "O", true)
+    @players = [Player.new("user", "X", false), Player.new("bot", "O", true)]
+  end
+
+  def play
+    puts "Starting game."
+    @board.show
+    i = 0
+    while @round <= 9
+      current_player = @players[i]
+
+      puts "#{ current_player.name}'s turn. Select your move:"
+      move = current_player.choose_move(@board.available_moves)
+      @board.update(move, current_player.symbol)
+      @board.show
+      
+      @round += 1
+
+      i = i == 0 ? 1 : 0
+    end
   end
 end
+
+#LOOP while nobody has won and there are turns left.
+# Show the board.
+# IF it's player1's turn, ask for their input.
+#   Modify board with player1's input. 
+# ELSE IF it's player2's turn, asl for their input. 
+#   Modify board with player2's input. 
 
 # The main board.
 class Board < Game
@@ -31,6 +55,10 @@ class Board < Game
 
   def available_moves
     @board.reject { |slot| ["X", "O"].include?(slot) }
+  end
+
+  def update(move, symbol)
+    @board[move - 1] = symbol
   end
 
   #Start calling this method when @round >= 5.
@@ -65,14 +93,13 @@ class Player < Game
 
   # Available is an array of slots that can still be played.
   def choose_move(available)
-    if bot
-      available.sample
-    else
-      gets.to_i
-    end
+    play = bot ? available.sample : gets.to_i
+    puts "#{name} plays #{play}"
+    play
   end
 end
 
 game = Game.new()
+game.play
 
 #TODO(Turbocep): Create Board and Players from Game class. No user input for player details currently. Create players => call player for input => get input, change board, change round, get other player's input. You can base whose turn it is based on odd and even numbers. The first player created starts first. 
