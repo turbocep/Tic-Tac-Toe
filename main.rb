1bdb5d9f59d5effed0cba30b0637d00ba1540ef8
turbocep <162988238+turbocep@users.noreply.github.com>
2024-08-09 13:50:45 +0200

    lint with RuboCop.

--- main.rb
+++ main.rb
@@ -1,4 +1,3 @@
-# rubocop: disable all
 # frozen_string_literal: true
 
 # The game class.
@@ -6,41 +5,39 @@ class Game
   def initialize
     @round = 1
     @board = Board.new([1, 2, 3, 4, 5, 6, 7, 8, 9])
-    @players = [Player.new("user", "X", false), Player.new("bot", "O", true)]
+    @players = [Player.new('user', 'X', false), Player.new('bot', 'O', true)]
   end
 
   def play
-    puts "Starting game."
+    puts 'Starting game.'
     @board.show
     i = 0
     won = false
     while @round <= 9
       current_player = @players[i]
 
-      puts "#{ current_player.name}'s turn. Select your move:"
+      puts "#{current_player.name}'s turn. Select your move:"
       move = current_player.choose_move(@board.available_moves)
       @board.update(move, current_player.symbol)
       @board.show
 
-      i = i == 0 ? 1 : 0
+      i = i.zero? ? 1 : 0
 
-      if @round >= 5
-        won = @board.check_for_wins
-      end
+      won = @board.check_for_wins if @round >= 5
       puts @round
       @round += 1
       break if won
     end
-    puts "Game over."
+    puts 'Game over.'
   end
 end
 
-#LOOP while nobody has won and there are turns left.
+# LOOP while nobody has won and there are turns left.
 # Show the board.
 # IF it's player1's turn, ask for their input.
-#   Modify board with player1's input. 
-# ELSE IF it's player2's turn, asl for their input. 
-#   Modify board with player2's input. 
+#   Modify board with player1's input.
+# ELSE IF it's player2's turn, asl for their input.
+#   Modify board with player2's input.
 
 # The main board.
 class Board < Game
@@ -61,20 +58,20 @@ class Board < Game
   end
 
   def available_moves
-    @board.reject { |slot| ["X", "O"].include?(slot) }
+    @board.reject { |slot| %w[X O].include?(slot) }
   end
 
   def update(move, symbol)
     @board[move - 1] = symbol
   end
 
-  #Start calling this method when @round >= 5.
+  # Start calling this method when @round >= 5.
   def check_for_wins
     win_combos = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
 
     won = false
 
-    ["X", "O"].each do |symbol|
+    %w[X O].each do |symbol|
       win_combos.each do |combo|
         new_arr = []
         combo.each do |i|
@@ -108,7 +105,5 @@ class Player < Game
   end
 end
 
-game = Game.new()
+game = Game.new
 game.play
-
-#TODO(Turbocep): Create Board and Players from Game class. No user input for player details currently. Create players => call player for input => get input, change board, change round, get other player's input. You can base whose turn it is based on odd and even numbers. The first player created starts first. 
